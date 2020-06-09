d0fe34bd714c62197bbb51fee7e5c172c92bfaa5
boom100100 <bsd231@nyu.edu>
2020-06-09 10:09:22 -0400

    Added ansible workshop files.

---
+++ ansible/ansible-working/vagrant-agents/bootstrap.sh
@@ -0,0 +1,10 @@
+apt-get update
+apt-get install -y apache2
+# apt-get install -y software-properties-common
+# apt-add-repository --yes --update ppa:ansible/ansible
+# apt-get install -y python-netaddr
+# apt-get install -y ansible
+if ! [ -L /var/www ]; then
+  rm -rf /var/www
+  ln -fs /vagrant /var/www
+fi
